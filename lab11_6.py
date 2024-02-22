0d220c58eab7264ed8d0c781a509415a1eb478a5
66070273 <141387231+66070273@users.noreply.github.com>
2024-02-23 00:03:42 +0700

    Add files via upload

---
+++ lab11_6.py
@@ -0,0 +1,25 @@
+import json
+def bubble():
+    mylist = json.loads(input())
+    last = int(input())
+    c = 0
+    status = False
+    time = 0
+    while c <= last and status is False:
+        w = last
+        status = True
+        while w > c:
+            if ord((mylist[w])[0]) == ord((mylist[w - 1])[0]):
+                if int((mylist[w])[1:]) < int((mylist[w - 1])[1:]):
+                    status = False
+                    mylist[w], mylist[w - 1] = mylist[w - 1], mylist[w]
+            elif ord((mylist[w])[0]) < ord((mylist[w - 1])[0]):
+                status = False
+                mylist[w], mylist[w - 1] = mylist[w - 1], mylist[w]
+            time += 1
+            w -= 1
+        print(mylist)
+        c += 1
+    print("Comparison times:", time)
+
+bubble()
